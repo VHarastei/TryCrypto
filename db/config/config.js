1436442d85709a7e72b18936cd885cba672b5a4a
VHarastei <60541960+VHarastei@users.noreply.github.com>
2021-08-20 19:28:13 +0300

    heroku deploy

--- db/config/config.js
+++ db/config/config.js
@@ -20,6 +20,7 @@ module.exports = {
     username: process.env.PLANETSCALE_DB_USERNAME,
     password: process.env.PLANETSCALE_DB_PASSWORD,
     database: process.env.PLANETSCALE_DB,
+
     host: process.env.PLANETSCALE_DB_HOST,
     dialect: 'mysql',
   },

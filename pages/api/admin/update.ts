770270150d1b9799f3a072c41427f1d1560ec805
VHarastei <60541960+VHarastei@users.noreply.github.com>
2021-08-23 14:15:27 +0300

    fixed update db endpoint

--- pages/api/admin/update.ts
+++ pages/api/admin/update.ts
@@ -7,8 +7,9 @@ const db = require('db/models/index');
 
 const handler = nextConnect().get(async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const isAuth = req.headers.authorization === process.env.ADMIN_SECRET_KEY;
-    if (!isAuth) return res.status(403).send('unauthorized');
+    // const isAuth = req.headers.authorization === process.env.ADMIN_SECRET_KEY;
+    // if (!isAuth) return res.status(403).send('unauthorized');
+    // turned off because easycron can`t set headers with free plan
 
     const users = await db.User.findAll({
       attributes: ['id'],
@@ -38,23 +39,23 @@ const handler = nextConnect().get(async (req: NextApiRequest, res: NextApiRespon
 
         //const date = addDays(new Date(), 1);
         const date = new Date().toISOString();
-        // await db.Balance.create({
-        //   date, //new Date().toISOString(),
-        //   usdValue: balance,
-        //   historicalDataId,
-        // });
-        // await db.PNL.create({
-        //   date, //new Date().toISOString(),
-        //   usdValue: yesterdayBalance ? +(balance - yesterdayBalance.usdValue).toFixed(2) : 0,
-        //   historicalDataId,
-        // });
-        console.log('UPDATED');
-
-        return {
+        await db.Balance.create({
           date, //new Date().toISOString(),
           usdValue: balance,
           historicalDataId,
-        };
+        });
+        await db.PNL.create({
+          date, //new Date().toISOString(),
+          usdValue: yesterdayBalance ? +(balance - yesterdayBalance.usdValue).toFixed(2) : 0,
+          historicalDataId,
+        });
+        console.log('UPDATED');
+
+        // return {
+        //   date, //new Date().toISOString(),
+        //   usdValue: balance,
+        //   historicalDataId,
+        // };
       })
     );
 

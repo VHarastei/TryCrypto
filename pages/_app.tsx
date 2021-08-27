d0d69f9f461147ee321a768e8ec78ca3b969f6db
VHarastei <60541960+VHarastei@users.noreply.github.com>
2021-08-27 14:52:03 +0300

    fixed bugs and removed currency seeder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import type { AppContext, AppProps } from 'next/app';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import Router from 'next/router';
-
 import { wrapper } from '../store';
 import App from 'next/app';
+import React from 'react';
+import Head from 'next/head';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -18,7 +19,15 @@ class MyApp extends App {
   }
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head>
+          <title>TryCrypto</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
   }
 }
 
